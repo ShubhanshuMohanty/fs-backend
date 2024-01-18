c08c36ce6e8b95165631ff2db7d5254e2279b386
Shubhanshu Mohanty <shubhanshumohanty@gmail.com>
2024-01-18 11:00:53 +0530

    access token,refresh token ,middleware,cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import {User} from '../models/user.model.js'
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+
+const generateAccessAndRefreshToken= async (userId)=>
+{
+    try {
+        
+        const user=await User.findById(userId)
+        const accessToken= user.generateAccessToken();
+        const refreshToken=user.generateRefreshToken();
+
+        user.refreshToken=refreshToken;
+        await user.save({validateBeforeSave: false})
+        
+        return {accessToken,refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,"something went wrong while generating refreshing access token");
+    }
+}
+
 const registerUser= asyncHandler(async (req,res)=>{
     
     const{fullName,email,username,password}=req.body
@@ -66,4 +85,93 @@ const registerUser= asyncHandler(async (req,res)=>{
     )
 })
 
-export {registerUser}
+const loginUser= asyncHandler(async(req,res)=>{
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+    const {email,username,password}=req.body;
+
+    if(!username || !email)
+    {
+        throw new ApiError(400, "username or email is required" )
+    }
+
+    const user=await User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(!user)
+    {
+        throw new ApiError(400,"user not found");
+    }
+
+    const isPasswordValid=await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid)
+    {
+        throw new ApiError(401,"password is incorrect");
+    }
+
+    const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser= await User.findById(user._id).select("-password -refreshToken");
+
+    const options={ //isase koi bhi bfrontend me cookies ko change nahi kar payega ,cookies ko bas dekh sakte ,change bas server side se hogas c
+        httpOnly : true ,
+        secure: true,
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "user logged in successfully"
+        )
+    )
+
+})
+
+const logoutUser= asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        { //isase new updated value milegi
+            new :true
+        }
+    )
+
+    const options={ //isase koi bhi bfrontend me cookies ko change nahi kar payega ,cookies ko bas dekh sakte ,change bas server side se hogas c
+        httpOnly : true ,
+        secure: true,
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "user logged out successfully"
+        )
+    )
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,12 +51,16 @@ const userSchema = new Schema(
         timestamps: true
     }
 );
+
+// data save hone se just pahale iss pre hook call karwa sakte
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next;
     this.password=await bcrypt.hash(this.password,10)
     next()
 })
 
+
+//.methods se userdefine methods bana sakte
 userSchema.methods.isPasswordCorrect= async function(password){
     return await bcrypt.compare(password, this.password)
 }
